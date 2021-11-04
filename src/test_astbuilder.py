47d140e40d83fddfcdd47485439f2b5f39c6203b
Carl Friedrich Bolz-Tereick <cfbolz@gmx.de>
2021-11-04 12:43:05 +0100

    deal with repeated keywords

--- src/test_astbuilder.py
+++ src/test_astbuilder.py
@@ -1133,8 +1133,8 @@ class TestAstBuilder:
         self.get_ast(input) # doesn't crash any more
         exc = pytest.raises(SyntaxError, self.get_ast, "f((a+b)=c)").value
         assert exc.msg == 'expression cannot contain assignment, perhaps you meant "=="?'
-        #exc = pytest.raises(SyntaxError, self.get_ast, "f(a=c, a=d)").value # XXX
-        #assert exc.msg == "keyword argument repeated: 'a'"
+        exc = pytest.raises(SyntaxError, self.get_ast, "f(a=c, a=d)").value
+        assert exc.msg == "keyword argument repeated: 'a'"
         with pytest.raises(SyntaxError) as excinfo:
             self.get_ast("f((x)=1)")
         assert excinfo.value.msg == 'expression cannot contain assignment, perhaps you meant "=="?'
